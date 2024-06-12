4a03c04656f7ac152d252665d190a6f5dc2f03a9
Lukekks <69133217+Lukekks@users.noreply.github.com>
2024-06-11 23:56:15 -0300

    Delete StudyPal directory

--- StudyPal/scripts/forum.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const publishArticleBtn = document.getElementById('publishArticleBtn');
-
-    publishArticleBtn.addEventListener('click', function() {
-        alert('Redirecionando para a página de publicação de artigos...');
-        window.location.href = '../pages/publicarArtigo.html';
-    });
-});
